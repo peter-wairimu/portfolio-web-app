4ea1f2cdd654352e1d24fa8388de6f7edd7c48e6
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-10 21:17:30 +0300

    update service component & media queries

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './services.css'
+import {BsCheck} from 'react-icons/bs'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Do</h5>  
+      <h2>My Services</h2>
+      <div className='container services-container'>
+        <article className='service'>
+          <div className="service_head">
+            <h3>UI/UX Designer</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+
+          </ul>
+
+        </article>
+
+          <article className='service'>
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service_list'>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+            <li>
+              <BsCheck className="service_icon" />
+              <p>
+                I design and develop user-friendly and intuitive interfaces for web and mobile applications.
+              </p>
+            </li>
+
+          </ul>
+
+        </article>
+        
+       </div>
+    
+    </section>
   )
 }
 
